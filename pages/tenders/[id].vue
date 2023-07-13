cb2a18e4ba8f9bef83de73d7ba7325b30c911993
4upik17 <nikita@etrusty.io>
2023-07-14 00:57:31 +0300

    Add single page for tender

--- pages/tenders/[id].vue
+++ pages/tenders/[id].vue
@@ -1,12 +1,62 @@
 <template>
   <div class="container steps-form">
     <div class="columns">
-      <div class="column box">
+      <div class="column menu-container box is-4">
+        <div class="content steps-menu">
+          <ul>
+            <li
+              v-for="(item, idx) in menuItems"
+              :key="item"
+              :class="activeItem == idx ? 'is-active' : ''"
+            >
+              <a @click="choseMenuItem(idx)">{{ item }}</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="column is-8 box">
         <div v-if="pending" class="loader-wrapper is-active">
           <div class="loader is-loading"></div>
         </div>
         <div v-if="!pending" class="tender-info">
-          <h3 class="title is-4">{{ tender.title }}</h3>
+          <div v-if="activeItem == 0" class="tender-info-section">
+            <h3 class="title is-4">{{ tender.title }}</h3>
+            <div class="tender-info-option">
+              <span class="icon">
+                <i class="fa fa-circle-info"></i>
+              </span>
+              <span>Draft</span>
+            </div>
+            <div class="tender-info-option description">
+              <span class="icon">
+                <i class="fa fa-comment"></i>
+              </span>
+              <span
+                >Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the industry's
+                standard dummy text ever since the 1500s, when an unknown
+                printer took a galley of type and scrambled it to make a type
+                specimen book. It has survived not only five centuries, but also
+                the leap into electronic typesetting, remaining essentially
+                unchanged. It was popularised in the 1960s with the release of
+                Letraset sheets containing Lorem Ipsum passages, and more
+                recently with desktop publishing software like Aldus PageMaker
+                including versions of Lorem Ipsum.</span
+              >
+            </div>
+            <div class="tender-info-option">
+              <span class="icon">
+                <i class="fa fa-envelope"></i>
+              </span>
+              <span>asd@example.com</span>
+            </div>
+            <div class="tender-info-option">
+              <span class="icon">
+                <i class="fa fa-building"></i>
+              </span>
+              <span>prisma</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -14,7 +64,7 @@
 </template>
 
 <script setup lang="ts">
-import { nextTick } from "vue";
+import { nextTick, ref } from "vue";
 import { useUserStore } from "@/stores/user";
 
 const route = useRoute();
@@ -25,6 +75,8 @@ const config = useRuntimeConfig();
 const baseURL = config.public.baseURL;
 const token = store.token;
 const tenderId = route.params.id;
+const activeItem = ref(0);
+const menuItems = ["Information", "Lot information", "Lot criterions"];
 
 const { data: tender, pending } = useFetch(
   () => `${baseURL}/api/v1/tenders/${tenderId}`,
@@ -36,11 +88,34 @@ const { data: tender, pending } = useFetch(
     transform: (result) => result.data,
   }
 );
+
+function choseMenuItem(idx) {
+  activeItem.value = idx;
+}
 </script>
 
 <style lang="scss">
 .user-info {
   font-size: 18px;
   margin-bottom: 10px;
+}
+
+.tender-info-option {
+  font-size: 18px;
+  color: #273038;
+  margin-bottom: 10px;
+
+  &.description {
+    display: flex;
+
+    .icon {
+      margin-right: 10px;
+    }
+  }
+
+  .icon {
+    margin-right: 5px;
+    justify-content: left;
+  }
 }
 </style>
